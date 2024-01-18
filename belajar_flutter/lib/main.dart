2c556b43c02a97eee68594b1f18694f6f1daba8e
unknown <cindyauliani0629@gmail.com>
2024-01-18 09:01:28 -0300

    flutter

---
+++ belajar_flutter/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'container_widget.dart';
+import 'colum_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Belajar Flutter",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("CINDY"),
+          backgroundColor: const Color.fromARGB(255, 233, 126, 162),
+          centerTitle: true,
+        ),
+        body: Container(
+          color: Colors.blue,
+          child: ContainerWidget(
+
+          ),
+        ),
+      ),
+      
+    );
+  }
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Hello World",
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+        color: Colors.green[400],
+      ),
+      ),
+      
+    );
+  }
+}
